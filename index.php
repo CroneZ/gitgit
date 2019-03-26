6a8d4dcaaa5ff632968ec2be0dcc3fa47fae69ab
CroneZ <sowcw1@hotmail.com>
2019-03-26 15:05:35 +0800

    commits 3

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<body>
-<h1>List of Burdens</h1>
-<h2>1.Lai Zi Wei</h2>
-<h2>2.Sow Chen Wei</h2>
-<h2>Thank you Hong Zhen Yue</h2>
-</body>
+<!DOCTYPE HTML5>
+<html>
+  <head><title>Welcome Users</title></head>
+  <body>
+    <div class = "navBar">
+      <a>Home</a>
+    </div>
+  </body>
+
+</html>
