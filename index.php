08718864fc88972b5ae332b280b2fec01826a055
CroneZ <sowcw1@hotmail.com>
2019-03-26 15:20:10 +0800

    commits 7 - add body and script

--- index.php
+++ index.php
@@ -40,4 +40,38 @@
       </div>
     </div>
 </head>
+<body></body>
+<script>
+  function openForm(){
+    // document.getElementById("filter").style.opacity = 1;
+    document.getElementById("filter").className = "filter3";
+    document.getElementById("topRightButton").className = "logout2";
+  }
+  function closeForm(){
+    // document.getElementById("filter").style.opacity = 0;
+    document.getElementById("filter").className = "filter2";
+    document.getElementById("topRightButton").className = "logout";
+  }
+  function checkLogin(){
+  	<?php
+  		if(isset($_SESSION['success'])){
+  			$checkLoggedIn = $_SESSION['success'];
+  			if(isset($_SESSION['userID'])){$userID = $_SESSION['userID'];}
+  		}
+  	?>
+  	var checkLoggedIn = "<?php if(isset($checkLoggedIn)){echo $checkLoggedIn;}else{/*This is the default case*/echo 'false';}?>";
+  	if(checkLoggedIn == true){
+  		var user = "<?php if(isset($userID)){echo $userID;}else{ echo 'default';} ?>";
+  		document.getElementById("topRightButton").text = "LogOut";
+  		document.getElementById("topRightButton").href = "index.php?action=logout";
+  		document.getElementById("welcomeText").innerHTML = "Welcome, "+user;
+  	}else{
+  		document.getElementById("topRightButton").text = "LogIn";
+  		document.getElementById("topRightButton").href = "javascript:openForm()";
+  		document.getElementById("welcomeText").innerHTML = "";
+  	}
+  }
+
+</script>
+
 </html>
